e45894280b405c66461c0225395995afb193ed50
StevenPan1234 <>
2021-05-09 07:14:57 +0000

    ...

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ let number = 0
 function cn () {
   number = document.getElementById('input').value
   number = parseInt(number)
-  if (number < 0) { document.getElementById('p').innerHTML = "negative" } else { document.getElementById('p').innerHTML = "positive" }
+  if (number < 0) { document.getElementById('p').innerHTML = 'negative' } else { document.getElementById('p').innerHTML = 'positive' }
 }
